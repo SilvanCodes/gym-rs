b65da552d2b348fecf35d12c092ba5bc747d0685
Silvan Buedenbender <silvan@claiv.de>
2021-02-02 18:15:51 +0100

    Run cargo fmt

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,21 +1,21 @@
 extern crate gym;
 
 fn main() {
-	let gym = gym::GymClient::default();
-	let env = gym.make("CartPole-v1");
+    let gym = gym::GymClient::default();
+    let env = gym.make("CartPole-v1");
 
-	for _ in 0..10 {
-		env.reset().expect("Unable to reset");
+    for _ in 0..10 {
+        env.reset().expect("Unable to reset");
 
-		for _ in 0..100 {
-			let action = env.action_space().sample();
-			let state = env.step(&action).unwrap();
-			env.render();
-			if state.is_done {
-				break;
-			}
-		}
-	}
+        for _ in 0..100 {
+            let action = env.action_space().sample();
+            let state = env.step(&action).unwrap();
+            env.render();
+            if state.is_done {
+                break;
+            }
+        }
+    }
 
-	env.close();
+    env.close();
 }
